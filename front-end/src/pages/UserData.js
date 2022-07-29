82c8f989c99989db32ae55cb968b195455305141
DanteAlonsoHT <dante1818@hotmail.com>
2022-07-29 16:56:35 -0500

    Update Dockerfile and some issues
    Add npm install as default in dockerfile
    Remove unnecesary functions

--- front-end/src/pages/UserData.js
+++ front-end/src/pages/UserData.js
@@ -5,15 +5,6 @@ import { connect, useDispatch } from "react-redux";
 
 import { createSecretAction } from "../store/actions/SecretActions";
 
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if (new Date().getTime() - start > milliseconds) {
-      break;
-    }
-  }
-}
-
 const UserData = () => {
   const [secret, setSecret] = useState({ secret: "" });
   const [errors, setErrors] = useState({ secret: "" });
